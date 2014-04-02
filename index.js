f6183040d83079e0d08e6cf94972b207823e9773
Tony <tony@rasingthefloor.org>
2014-04-02 12:49:16 +0200

    If a user updates their own record, their session information needs to be updated.

--- index.js
+++ index.js
@@ -299,6 +299,12 @@ module.exports = function(config) {
         }
         db.insert(user, 'org.couchdb.user:' + req.params.name, function(err, updatedUser) {
             if (err) { return res.send(err.status_code, err); }
+
+            // If a user updates their record, we need to update the session data
+            if (req.session.user.name === req.params.name) {
+                req.session.user = updatedUser;
+            }
+
             return res.send(200, JSON.stringify({ok: true, user: strip(updatedUser) }));
         });
     });
